8801187296b489d9fd319024ddab5188a684dd89
Surya Kanta Ghosh <suryaghosh1406@gmail.com>
2023-01-16 01:56:23 +0530

    blank canvas

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return ( 
+        <div>
+            <p className="text-2xl font-bold p-5">Surya Kanta Ghosh</p>
+            <hr />
+        </div>
+     );
+}
+ 
+export default Header;
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from '../Components/Header'
+
+const Home = () => {
+    return ( 
+        <div className="w-11/12 mx-auto">
+            <Header />
+        </div>
+     );
+}
+ 
+export default Home;
