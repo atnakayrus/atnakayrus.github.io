fa6f6d6729454c369a8af8b1cad1041fba4a6e60
Surya Kanta Ghosh <52543164+atnakayrus@users.noreply.github.com>
2023-04-09 06:56:30 +0530

    footer alignment

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,27 +2,26 @@ import github from '../assets/github.png'
 import fiverr from '../assets/fiverr.png'
 import instagram from '../assets/instagram.png'
 import linkedin from '../assets/linkedin.png'
+import FooterIcon from '../widgets/footerImageIcon'
 
 const Footer = () => {
     return (
         <>
             <div className="Footer flex flex-row">
                 <div className="footer-left flex flex-row w-full overflow-auto flex-wrap">
-                    <div className='heading1 w-full flex'>
-                        <text className="text-2xl font-bold">Atnakayrus</text>
-                    </div>
-                    <div className="leftbox w-1/2 flex flex-col">
-                        <text className="text-lg font-bold">Reach out to me at:</text>
-                        <text>suryaghosh1406@gmail.com</text>
-                        <text>+91 9718172763</text>
+                    <div className="leftbox w-1/2 flex flex-col ">
+                    <text className="text-2xl font-bold m-auto">Atnakayrus</text>
+                        <text className="text-lg font-bold m-auto">Reach out to me at:</text>
+                        <text className='m-auto'>suryaghosh1406@gmail.com</text>
+                        <text className='m-auto'>+91 9718172763</text>
                     </div>
                     <div className="w-1/2 rightbox flex flex-col">
-                        <text>Follow me at</text>
-                        <div className="Icons flex-row flex">
-                            <img src={github} alt='github'></img>
-                            <img src={fiverr} alt='github'></img>
-                            <img src={linkedin} alt='github'></img>
-                            <img src={instagram} alt='github'></img>
+                        <text className='flex m-auto'>Follow me at</text>
+                        <div className="Icons flex-row flex m-auto">
+                            <FooterIcon src={github} alt='github' href='#'></FooterIcon>
+                            <FooterIcon src={linkedin} alt='linkedin' href='#'></FooterIcon>
+                            <FooterIcon src={fiverr} alt='fiverr' href='#'></FooterIcon>
+                            <FooterIcon src={instagram} alt='instagram' href='#'></FooterIcon>
                         </div>
                     </div>
                 </div>
