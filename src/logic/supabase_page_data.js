fd319cdc489a0d7d1c0846fc0f5a045122ce4cff
Surya Kanta Ghosh <52543164+atnakayrus@users.noreply.github.com>
2025-06-26 00:20:33 +0530

    experience

--- src/logic/supabase_page_data.js
+++ src/logic/supabase_page_data.js
@@ -13,10 +13,16 @@ const loadAboutPage = async ()=>{
   return(data)
 }
 
+const loadExperiencePage = async ()=>{
+  let { data : work_data } = await supabase.from("experience").select("*").eq("type","work");
+  let { data : personal_data } = await supabase.from("experience").select("*").eq("type","personal");
+  return {work_data,personal_data}
+}
+
 const loadFooterData = async ()=>{
   let { data: page_data } = await supabase.from("page_data").select("*").eq('page','global');
   const data = flatten(page_data)
   return({'email':data.email,'github':data.github,'linkedin':data.linkedin})
 }
 
-export { loadHomepage , loadFooterData ,loadAboutPage};
+export { loadHomepage , loadFooterData ,loadAboutPage,loadExperiencePage};
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,5 +1,70 @@
+import { useEffect, useState } from "react";
+import { loadExperiencePage } from "../logic/supabase_page_data";
+
 const Experience = () => {
-    return ( <></> );
-}
- 
+  const [workExp, setWorkExp] = useState(null);
+  const [personalExp, setPersonalExp] = useState(null);
+
+  useEffect(() => {
+    loadPage();
+  }, []);
+
+  const loadPage = async () => {
+    const { work_data, personal_data } = await loadExperiencePage();
+    console.log(work_data, "work");
+    console.log(personal_data, "personal");
+    setWorkExp(work_data);
+    setPersonalExp(personal_data);
+  };
+
+  return (
+    <div className="Experience">
+      <div className="shadowdown">
+        <h1 className="heading">WORK EXPERIENCE</h1>
+      </div>
+      <div className="container">
+        {workExp &&
+          workExp.map((exp) => {
+            return (
+              <>
+                <div className="titlebox">
+                  <p className="title">{exp.title}</p>
+                  <p className="timeline">{exp.timeline}</p>
+                </div>
+                {exp.points && 
+                <ol>
+                  {exp.points.map((point) => {
+                    return <li>● {point}</li>;
+                  })}
+                </ol>}
+              </>
+            );
+          })}
+      </div>
+      <div className="shadowdown">
+        <h1 className="heading">POSITION OF RESPONSIBILTY</h1>
+      </div>
+      <div className="container">
+        {personalExp &&
+          personalExp.map((exp) => {
+            return (
+              <>
+                <div className="titlebox">
+                  <p className="title">{exp.title}</p>
+                  <p className="timeline">{exp.timeline}</p>
+                </div>
+                {exp.points && 
+                <ol>
+                  {exp.points.map((point) => {
+                    return <li>● {point}</li>;
+                  })}
+                </ol>}
+              </>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
 export default Experience;
